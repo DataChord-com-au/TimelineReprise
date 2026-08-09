c2f7b2daa9667146d0ea8209560e66a49cd99700
DataChord <220486922+DataChord-com-au@users.noreply.github.com>
2026-08-10 00:06:17 +0930

    incorporate latest build

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 const Reprise = Object.freeze({
     // timeline-reprise-version:start
-    version: "3.3.11",
+    version: "3.3.12",
     // timeline-reprise-version:end
     timelineVersion: "2.3.1"
 });
